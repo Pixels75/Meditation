f2e9cba5d731edefb38e439185d5484cc6e63bab
Pixels <164868626+Pixels75@users.noreply.github.com>
2024-09-10 17:57:30 +0300

    Add Basic Gameplay

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private KeyCode inputKey;
+    private int _score;
+    private bool _isOverlapping;
+    
+    private void Update()
+    {
+        Debug.Log( "Score: " + _score );
+        if ( Input.GetKeyDown( inputKey ) && !_isOverlapping )
+        {
+            _score--;
+        }
+    }
+
+    private IEnumerator CheckForInput()
+    {
+        var keyPressed = false;
+        while ( _isOverlapping && !keyPressed )
+        {
+            yield return null;
+            if ( !Input.GetKeyDown( inputKey ) ) continue;
+            keyPressed = true;
+            _score++;
+        }
+    }
+
+    private void OnTriggerEnter2D( Collider2D other )
+    {
+        _isOverlapping = true;
+        StartCoroutine( CheckForInput() );
+    }
+    private void OnTriggerExit2D( Collider2D other )
+    {
+        _isOverlapping = false;
+    }
+}
