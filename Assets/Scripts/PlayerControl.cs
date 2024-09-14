6a01f62a2412104e6640e2acfae18a87e1695a13
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-09-14 19:19:13 -0400

    Added invinciblity

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent( typeof( Rigidbody2D ) )]
-[RequireComponent( typeof( Circle ) )]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Circle))]
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private KeyCode inputKey;
     [SerializeField] public float thunderTimeframe;
     [SerializeField] private int lives;
+    [SerializeField] private float invincibility;
     [Header("Acceleration")]
     [SerializeField] private float missAcceleration;
     [SerializeField] private float wrongPressAcceleration;
@@ -17,93 +18,110 @@ public class PlayerControl : MonoBehaviour
     [Header("Score")]
     [SerializeField] private int thunderBonusScore;
     [SerializeField] private int hitScore;
-    
+
     private Circle _circle;
     private bool _isOverlapping;
     private bool _thunderStrikeActive;
+    private bool _isInvincible;
 
     private void Awake()
     {
         _circle = GetComponent<Circle>();
     }
+
     private void Start()
     {
         GameManager.Instance.ChangeLives(lives);
     }
+
     private void Update()
     {
-
-        if ( Input.GetKeyDown( inputKey ) && !_isOverlapping && !_thunderStrikeActive )
+        if (Input.GetKeyDown(inputKey) && !_isOverlapping && !_thunderStrikeActive && !_isInvincible)
         {
-            lives -= 1;
-            GameManager.Instance.ChangeLives( -1 );
-            AudioManager.Instance.PlaySound( "Bad" );
-            _circle.Accelerate( wrongPressAcceleration );
+            LoseLife();
+            _circle.Accelerate(wrongPressAcceleration);
         }
     }
 
     public void Thunder()
     {
-        StartCoroutine( CheckForThunderInput( thunderTimeframe ) );
+        StartCoroutine(CheckForThunderInput(thunderTimeframe));
     }
 
-    private void OnTriggerEnter2D( Collider2D other )
+    private void OnTriggerEnter2D(Collider2D other)
     {
         _isOverlapping = true;
-        StartCoroutine( CheckForInput() );
+        StartCoroutine(CheckForInput());
     }
-    private void OnTriggerExit2D( Collider2D other )
+
+    private void OnTriggerExit2D(Collider2D other)
     {
         _isOverlapping = false;
     }
 
-    private IEnumerator CheckForThunderInput( float time )
+    private IEnumerator CheckForThunderInput(float time)
     {
         _thunderStrikeActive = true;
-        
+
         bool keyPressed = false;
         float timer = 0f;
 
-        while ( timer < time )
+        while (timer < time)
         {
             yield return null;
             timer += Time.deltaTime;
 
-            if ( !Input.GetKeyDown( inputKey ) ) continue;
+            if (!Input.GetKeyDown(inputKey)) continue;
             keyPressed = true;
-            GameManager.Instance.ChangeScore( thunderBonusScore );
+            GameManager.Instance.ChangeScore(thunderBonusScore);
             AudioManager.Instance.PlaySound("ThunderBonus");
             break;
         }
 
-        if ( !keyPressed )
+        if (!keyPressed)
         {
-            _circle.Accelerate( thunderAcceleration ); // Speed up if the key wasn't pressed
+            _circle.Accelerate(thunderAcceleration); // Speed up if the key wasn't pressed
         }
 
         _thunderStrikeActive = false;
     }
+
     private IEnumerator CheckForInput()
     {
         var keyPressed = false;
-        while ( _isOverlapping && !keyPressed )
+        while (_isOverlapping && !keyPressed)
         {
             yield return null;
-            if ( !Input.GetKeyDown( inputKey ) ) continue;
+            if (!Input.GetKeyDown(inputKey)) continue;
             keyPressed = true;
-            GameManager.Instance.ChangeScore( hitScore );
-            int rand = Random.Range( 1, 4 );
-            char index = ( char )( rand + 48 ); // Use ASCII trick to convert from int to char
-            AudioManager.Instance.PlaySound( "Good" + index );
-            _circle.Accelerate( -hitDeceleration );
+            GameManager.Instance.ChangeScore(hitScore);
+            int rand = Random.Range(1, 4);
+            char index = (char)(rand + 48); // Use ASCII trick to convert from int to char
+            AudioManager.Instance.PlaySound("Good" + index);
+            _circle.Accelerate(-hitDeceleration);
         }
 
-        if ( !keyPressed )
+        if (!keyPressed)
         {
-            lives -= 1;
-            // if the key wasn't pressed reduce score
-            GameManager.Instance.ChangeLives( -1 );
-            _circle.Accelerate( missAcceleration );
+            LoseLife();
+            _circle.Accelerate(missAcceleration);
         }
+    }
+
+    private void LoseLife()
+    {
+        if (_isInvincible) return; // Prevent life loss if invincible
+
+        lives -= 1;
+        GameManager.Instance.ChangeLives(-1);
+        AudioManager.Instance.PlaySound("Bad");
+        StartCoroutine(InvincibilityCooldown());
+    }
+
+    private IEnumerator InvincibilityCooldown()
+    {
+        _isInvincible = true;
+        yield return new WaitForSeconds(invincibility);
+        _isInvincible = false;
     }
 }
